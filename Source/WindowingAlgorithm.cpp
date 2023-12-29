afcfa5fa2bced76b7effee60e0cc50799f50d755
KaPlaneta <planeta.karolinaa@gmail.com>
2023-12-29 21:37:50 +0100

    feat: change slider value and simplify windowing algorithm

--- Source/WindowingAlgorithm.cpp
+++ Source/WindowingAlgorithm.cpp
@@ -13,89 +13,41 @@ WindowingAlgorithm::WindowingAlgorithm(unsigned int& WidthIt, unsigned int& Heig
 {
 }
 
+
+
+float calculateWindow(int windowSize, int currentHeight, int currentWidth, std::shared_ptr<juce::Image::BitmapData>imageBitmapPtr){
+    float sum = 0;
+
+    for (int x = 0; x < windowSize; ++x) {
+        for (int y = 0; y < windowSize; ++y) {
+            
+            auto pix_c =  imageBitmapPtr->getPixelColour(currentWidth + y, currentHeight + x);
+            float average = (pix_c.getFloatRed() + pix_c.getFloatGreen() + pix_c.getFloatBlue()) / 3.f;
+            sum += average;
+            
+        }
+    }
+    return sum;
+}
+
+
 void WindowingAlgorithm::generate_next_samples(float* output_buffer, unsigned int buffer_length, int windowSize)
 {
+    
+    
     int k = 0;
     int height = imageBitmapPtr->height;
     int width = imageBitmapPtr->width;
     
-    if (windowSize > height || windowSize > width) {
-        int newWindowSize = (height > width) ? width : height;
-        
-        
-        while (true) {
-            if (height > width) {
-                
-                for (; HeightIt < height - newWindowSize + 1; ++HeightIt) {
-                    float sum = 0;
-                    
-                    for (int x = 0; x < newWindowSize; ++x) {
-                        for (int y = 0; y < newWindowSize; ++y) {
-                            
-                            auto pix_c =  imageBitmapPtr->getPixelColour(WidthIt + y, HeightIt + x);
-                            float average = (pix_c.getFloatRed() + pix_c.getFloatGreen() + pix_c.getFloatBlue()) / 3.f;
-                            sum += average;
-                            
-                        }
-                    }
-                    if (k < buffer_length) {
-                        *(output_buffer + k) = sum / (newWindowSize * newWindowSize);
-                        
-                        k++;
-                    }
-                    else{
-                        return;
-                    }
-                }
-                HeightIt = 0;
-                
-            } else {
-                
-                for (; WidthIt < width - newWindowSize + 1; ++WidthIt) {
-                    float sum = 0;
-                    
-                    for (int x = 0; x < newWindowSize; ++x) {
-                        for (int y = 0; y < newWindowSize; ++y) {
-                            
-                            auto pix_c =  imageBitmapPtr->getPixelColour(WidthIt + y, HeightIt + x);
-                            float average = (pix_c.getFloatRed() + pix_c.getFloatGreen() + pix_c.getFloatBlue()) / 3.f;
-                            sum += average;
-                            
-                        }
-                    }
-                    if( k < buffer_length){
-                        *(output_buffer + k) = sum / (newWindowSize * newWindowSize);
-                        
-                        k++;
-                    }
-                    else{
-                        return;
-                    }
-                }
-                WidthIt = 0;
-            }
-        }
-    }
-        
+    
+     
     
     while (true) {
         for (; HeightIt < height - windowSize + 1; ++HeightIt) {
             
             for (; WidthIt < width - windowSize + 1; ++WidthIt) {
-                float sum = 0;
+                float sum = calculateWindow(windowSize, HeightIt, WidthIt,imageBitmapPtr);
                 
-               
-                
-                
-                for (int x = 0; x < windowSize; ++x) {
-                    for (int y = 0; y < windowSize; ++y) {
-                        
-                        auto pix_c =  imageBitmapPtr->getPixelColour(WidthIt + y, HeightIt + x);
-                        float average = (pix_c.getFloatRed() + pix_c.getFloatGreen() + pix_c.getFloatBlue()) / 3.f;
-                        sum += average;
-                        
-                    }
-                }
                 if( k < buffer_length){
                     *(output_buffer + k) = sum / (windowSize * windowSize);
                     
